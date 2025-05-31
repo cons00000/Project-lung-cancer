06fe71f37e927e6d16d6e60920311f137f1c4945
cons00000 <copiquet@gmail.com>
2025-05-31 22:43:01 +0200

    Xai

---
+++ src/Xai/xplique.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=gradcam_job
+#SBATCH --output=gradcam_%j.out
+#SBATCH --error=gradcam_%j.err
+#SBATCH --time=01:00:00            # 1 heure (ajuste selon ton besoin)
+#SBATCH --partition=gpu_inter            # ou la partition adaptée
+#SBATCH --mem=16G                  # mémoire (ajuste)
+#SBATCH --cpus-per-task=4          # CPU (ajuste)
+
+# Charge conda et active ton env
+conda init
+conda activate mon_env
+
+# Chemin vers ton notebook sur le cluster
+NOTEBOOK_PATH=/usr/users/pred_lung_cancer/piquet_con/Project-lung-cancer/src/Xai/Grad_Cam.ipynb
+
+# Lancer l’exécution du notebook avec papermill
+papermill $NOTEBOOK_PATH ${NOTEBOOK_PATH%.ipynb}_output.ipynb
